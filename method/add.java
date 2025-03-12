b42e434dfad011b83dc235cd5fd679295c602a99
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-12 14:34:29 +0530

    method is used

---
+++ method/add.java
@@ -0,0 +1,12 @@
+public class add {
+    void add(){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("the adding number:"+c);
+    }
+     public static void main(String arg[]){
+       add obj=new add();
+       obj.add();
+     }       
+}
