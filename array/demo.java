cc58a5b07ec388205a80e396c95b944be7b6a104
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-13 12:10:47 +0530

    array

---
+++ array/demo.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(String[] args) {
+        int a[]={10,20,30,40};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+    }
+    }
+
