037b5c01d4b18513a90b1209e119f145e5e038c2
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-13 11:02:40 +0530

    if else statement

---
+++ if_else_condition/file5.java
@@ -0,0 +1,11 @@
+public class file5{
+    public static void main(String[] args) {
+        boolean a=false;
+        if (a==true) {
+            System.out.println("the a value is true.");
+        }
+        else{
+            System.out.println("the a value is false.");
+        }
+    }
+}
