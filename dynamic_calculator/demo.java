66d3d44c94a4cde356771131124d05850423e1f1
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-13 15:43:06 +0530

    dynamic calculator

--- dynamic_calculator/demo.java
+++ dynamic_calculator/demo.java
@@ -15,26 +15,28 @@ public class demo {
             int d=b+c;
             System.out.println("the addition of two numbers:"+d);
         }
-        if(a==2){
+        else if(a==2){
             System.out.println("enter the input:");
             int num1=scan.nextInt();
             int num2=scan.nextInt();
             int num3=num1-num2;
             System.out.println("the sub of two numbers:"+num3);
         }
-        if(a==3){
+         else if(a==3){
             System.out.println("enter the input:");
             int num4=scan.nextInt();
             int num5=scan.nextInt();
             int num6=num4*num5;
             System.out.println("the multi of two numbers:"+num6);
         }
-        if(a==4){
+        else if(a==4){
             System.out.println("enter the input:");
             int num7=scan.nextInt();
             int num8=scan.nextInt();
             int num9=num7/num8;
             System.out.println("the divid of two numbers:"+num9);
+        }else{
+            System.out.println("worng command");
         }
     }
 }
