2aa02fccdbc6f595ee8f096a812e5f6c193a372e
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-13 11:19:58 +0530

    positive or negative

---
+++ positive_negative/file7.java
@@ -0,0 +1,10 @@
+public class file7{
+    public static void main(String[] args) {
+        int a=10;
+        if(a>0){
+            System.out.println("this number is positive.");
+        }else{
+            System.out.println("this number is negative.");
+        }
+    }
+}
