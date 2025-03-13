6286324867144f9c793f5f08a889ceca823496a4
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-13 11:56:07 +0530

    gender

---
+++ gender/file8.java
@@ -0,0 +1,10 @@
+public class file8{
+    public static void main(String[] args) {
+        String a="male";
+        if(a=="male"){
+            System.out.println("thi is male.");
+        }else{
+            System.out.println("this is female.");
+        }
+    }
+}
