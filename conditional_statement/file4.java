6a5f09fd2be984cc4c09ac55e75f44b8b9d01316
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-13 10:48:33 +0530

    conditional statement

---
+++ conditional_statement/file4.java
@@ -0,0 +1,10 @@
+public class file4{
+    public static void main(String[] args) {
+        boolean a=true;
+        if (a=true) {
+            System.out.println("the a value is true.");
+        }else{
+            System.out.println("the a value is false.");
+        }
+    }
+}
