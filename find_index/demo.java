ac941980a3609e22db6ea587b19a55f64e595c80
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-14 12:07:57 +0530

    fint the index of string

---
+++ find_index/demo.java
@@ -0,0 +1,7 @@
+public class demo{
+    public static void main(String[] args) {
+        String a="hello world";
+        System.out.println(a.indexOf("w"));
+        System.out.println(a.indexOf(" "));
+    }
+}
