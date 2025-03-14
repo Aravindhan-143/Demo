c8645e9c2f9fe90cb84cc274d89278a12a1f3f84
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-14 10:14:20 +0530

    find the n numbers

---
+++ find_n_number/demo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class demo {
+
+    public static void main(String[] args) {
+        int i =1;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter the input:");
+        int j=scan.nextInt();
+        while (i<=j) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
