adc69d98073c65aebe39e21444bdfd234d93423d
Aravindhan-143 <devildemon0608@gmail.com>
2025-03-14 11:51:57 +0530

    split method

---
+++ split/demo.java
@@ -0,0 +1,8 @@
+public class demo{
+    public static void main(String[] args) {
+        String a="hellow,world";
+        String b[]=a.split(",");
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+    }
+}
